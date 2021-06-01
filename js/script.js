e392156332415a535ed5244425a529c702bc2642
Sergiod3x <sergiod3x.@gmail.com>
2021-06-01 13:46:56 +0200

    zz

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ var num = 0;
 
 for (var i = 1; i < 101; i++) {
     if(((i%5)==0)&&((i%3)==0)){
-        num =  "fizBuzz <br>"  ;
+        num =  "FizzBuzz <br>"  ;
     }else{
         if ((i%5)==0){
             num =  "Buzz <br>"  ;
         } else{
             if((i%3)==0){
-                num =  "fiz <br>"  ;
+                num =  "Fizz <br>"  ;
             } else {
                 num = i +"<br>";
             }
